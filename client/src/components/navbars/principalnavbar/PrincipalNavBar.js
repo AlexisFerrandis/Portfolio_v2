4bbb4d8678e4107175f554e4ec9b28b13bd05c59
Alexis Ferrandis <alexis.1.ferrandis@gmail.com>
2022-07-27 16:14:28 +0200

    Principal Navbar style

--- client/src/components/navbars/principalnavbar/PrincipalNavBar.js
+++ client/src/components/navbars/principalnavbar/PrincipalNavBar.js
@@ -1,7 +1,44 @@
 import React from "react";
 
+//img
+import folder from "./../../../assets/navbars/principalnavbar/folder-yellow.svg";
+import console from "./../../../assets/navbars/principalnavbar/gnome-console.svg";
+import facebook from "./../../../assets/navbars/principalnavbar/facebook.svg";
+import netflix from "./../../../assets/navbars/principalnavbar/netflix.svg";
+import chat from "./../../../assets/navbars/principalnavbar/gnome-robots.svg";
+import games from "./../../../assets/navbars/principalnavbar/game-boy.svg";
+import calculator from "./../../../assets/navbars/principalnavbar/calculator.svg";
+import menu from "./../../../assets/navbars/principalnavbar/menu-opener.svg";
+
 const PrincipalNavBar = () => {
-	return <div className="principal-nav-bar">PrincipalNavBar</div>;
+	return (
+		<div className="principal-nav-bar">
+			<div className="principal-nav-bar__icon">
+				<img src={folder} alt="folder" />
+			</div>
+			<div className="principal-nav-bar__icon">
+				<img src={console} alt="console" />
+			</div>
+			<div className="principal-nav-bar__icon">
+				<img src={facebook} alt="facebook" />
+			</div>
+			<div className="principal-nav-bar__icon">
+				<img src={netflix} alt="netflix" />
+			</div>
+			<div className="principal-nav-bar__icon">
+				<img src={chat} alt="chat" />
+			</div>
+			<div className="principal-nav-bar__icon">
+				<img src={games} alt="video-games" />
+			</div>
+			<div className="principal-nav-bar__icon ">
+				<img src={calculator} alt="calculator" />
+			</div>
+			<div className="principal-nav-bar__icon menu-icon">
+				<img src={menu} alt="menu" />
+			</div>
+		</div>
+	);
 };
 
 export default PrincipalNavBar;
